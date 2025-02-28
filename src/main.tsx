071d08c26ada0ce3615f1cdb7a7950e7e253961c
ALEXANDR Kovalev <yakovalev1@mail.ru>
2025-02-28 13:00:30 +0300

    add base styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './app/providers/store/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/providers/router/router.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
