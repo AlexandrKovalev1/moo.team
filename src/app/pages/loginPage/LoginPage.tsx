9ed7391f441d933dd5bbb2f9bdbe82fb703179ac
ALEXANDR Kovalev <yakovalev1@mail.ru>
2025-03-01 09:44:37 +0300

    feat: create LoginPage.tsx

--- src/app/pages/loginPage/LoginPage.tsx
+++ src/app/pages/loginPage/LoginPage.tsx
@@ -1,9 +1,39 @@
 import Container from '../../components/Container/Container.tsx'
+import { TextField } from '../../components/textField/TextField.tsx'
+import { Button } from '../../components/Button/Button.tsx'
+import s from './loginPage.module.css'
+import { useFormik } from 'formik'
 
 export const LoginPage = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: values => {
+      console.log(values)
+      formik.resetForm()
+    },
+  })
+
   return (
     <div>
-      <Container></Container>
+      <Container>
+        <form className={s.form} onSubmit={formik.handleSubmit}>
+          <TextField
+            label={'Email address'}
+            placeholder={'Enter email'}
+            description={"we'll never share your emeil with enyone else"}
+            {...formik.getFieldProps('email')}
+          />
+          <TextField
+            label={'Password'}
+            placeholder={'Password'}
+            {...formik.getFieldProps('password')}
+          />
+          <Button type={'submit'}>Submit</Button>
+        </form>
+      </Container>
     </div>
   )
 }
