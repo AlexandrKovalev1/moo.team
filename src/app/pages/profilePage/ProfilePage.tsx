1219415afd430a5a463e06cef2ca8ecf3d83c058
ALEXANDR Kovalev <yakovalev1@mail.ru>
2025-03-01 12:29:11 +0300

    feat:create profile page

---
+++ src/app/pages/profilePage/ProfilePage.tsx
@@ -0,0 +1,19 @@
+import { Button } from '../../components/Button/Button.tsx'
+import avatar from '../../assets/images/avatar.jpg'
+import s from './profilePage.module.css'
+import Container from '../../components/Container/Container.tsx'
+
+export const ProfilePage = () => {
+  return (
+    <div>
+      <Container>
+        <div className={s.profileBlock}>
+          <img src={avatar} alt="avatar-image" className={s.avatar} />
+          <h1>Welcome,Alexey!</h1>
+          <Button>Update</Button>
+        </div>
+      </Container>
+      <div>здесь место для объединенного результата длительного вызова</div>
+    </div>
+  )
+}
