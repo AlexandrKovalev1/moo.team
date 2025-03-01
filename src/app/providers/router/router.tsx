84beb9f28ee6b8c521e0583a9f6636b6e7b4c4b8
ALEXANDR Kovalev <yakovalev1@mail.ru>
2025-03-01 12:31:42 +0300

    feat: make a private and public routes

--- src/app/providers/router/router.tsx
+++ src/app/providers/router/router.tsx
@@ -1,8 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate, Outlet, RouteObject } from 'react-router-dom'
 import { App } from '../../../App.tsx'
 import InfoPage from '../../pages/infoPage/ui/InfoPage.tsx'
-import { LoginPage } from '../../pages/loginPage/LoginPage.tsx'
+import { LoginPage } from '../../pages/loginPage/ui/LoginPage.tsx'
 import { ProfilePage } from '../../pages/profilePage/ProfilePage.tsx'
+import { useSelector } from 'react-redux'
+import { AppRootStateType } from '../store/store.ts'
 
 export const PATH = {
   ABOUT_US: '/info',
@@ -11,23 +13,34 @@ export const PATH = {
   PROFILE: '/profile',
 }
 
+export const ProtectedRoutes = () => {
+  const isAuth = useSelector<AppRootStateType, boolean>(state => state.auth.isAuth)
+
+  return isAuth ? <Outlet /> : <Navigate to={PATH.ABOUT_US} />
+}
+
+const privateRotes: RouteObject[] = [
+  {
+    path: PATH.PROFILE,
+    element: <ProfilePage />,
+  },
+]
+
+const publicRotes: RouteObject[] = [
+  {
+    path: PATH.ABOUT_US,
+    element: <InfoPage />,
+  },
+  {
+    path: PATH.SIGN_IN,
+    element: <LoginPage />,
+  },
+]
+
 export const router = createBrowserRouter([
   {
     path: PATH.BASE,
     element: <App />,
-    children: [
-      {
-        path: PATH.ABOUT_US,
-        element: <InfoPage />,
-      },
-      {
-        path: PATH.SIGN_IN,
-        element: <LoginPage />,
-      },
-      {
-        path: PATH.PROFILE,
-        element: <ProfilePage />,
-      },
-    ],
+    children: [{ element: <ProtectedRoutes />, children: privateRotes }, ...publicRotes],
   },
 ])
