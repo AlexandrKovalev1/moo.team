7d9f59bce5acf7a2add4f787291576cfa5e8072a
ALEXANDR Kovalev <yakovalev1@mail.ru>
2025-03-05 15:56:37 +0300

    feat:create getRandomAuthor and getRandomQuote functions

--- src/app/pages/profilePage/api/profileApi.ts
+++ src/app/pages/profilePage/api/profileApi.ts
@@ -2,26 +2,32 @@ import {
   profileResponse,
   ProfileResponse,
   RandomAuthorResponse,
+  RandomQuoteResponse,
 } from '../../../common/data/responseData.ts'
-import { autors } from '../../../common/data/appendixData.ts'
-import { useAppSelector } from '../../../providers/store/store.ts'
-
-const isCancelled = useAppSelector(state => state.profile.isCancelledPromise)
+import { autors, quotes } from '../../../common/data/appendixData.ts'
+import { getRandomIndex } from '../../../common/utils/getRandomIndex.ts'
 
 export const getProfileApi = () => {
   return new Promise<ProfileResponse>(resolve => resolve(profileResponse))
 }
 
 export const getRandomAuthor = () => {
-  const randomIndex = Math.floor(Math.random() * (autors.length + 1))
+  const randomIndex = getRandomIndex(autors.length)
+  return new Promise<RandomAuthorResponse>((resolve, reject) => {
+    if (randomIndex !== undefined) {
+      resolve({ success: true, data: autors[randomIndex] })
+    } else {
+      reject({ success: false, data: { message: 'Not authors' } })
+    }
+  })
+}
 
-  return new Promise<RandomAuthorResponse>((resolve, reject) =>
-    setTimeout(() => {
-      if (isCancelled) {
-        reject('Promise was cancelled')
-      } else {
-        resolve({ success: true, data: autors[randomIndex] })
-      }
-    }, 5000)
+export const getRandomQuote = (authorId: number) => {
+  const authorQuotes = quotes.filter(q => q.authorId === authorId)
+
+  const randomIndex = getRandomIndex(authorQuotes.length)
+
+  return new Promise<RandomQuoteResponse>(resolve =>
+    resolve({ success: true, data: authorQuotes[randomIndex] })
   )
 }
